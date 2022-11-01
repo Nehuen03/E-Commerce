5e8fa67507a4e9a2215f82d88de2f86ab30773f9
Nehuen03 <est.napoli.nehuen@latecnicalf.com.ar>
2022-11-01 19:21:44 -0300

    act

--- logindos.php
+++ logindos.php
@@ -11,10 +11,30 @@ include './conex.php';
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
 
-    <link rel="stylesheet" href="logindos.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
+    <section id="header">
+        <a href="#"><img src="img/logo.png" class="logo" alt="logo"></a>
+
+        <div>
+            <ul id="navbar">
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="shop.html">Productos</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="about.html">Nosotros</a></li>
+                <li><a href="contact.html">Contacto</a></li>
+                <li><a class="active" href="logindos.php">Login</a></li>
+                <li id="lg-bag"><a href="cart.html"><i class="fa-solid fa-bag-shopping"></i></a></li>
+                <a href="#" id="close"><i class="fa-solid fa-xmark"></i></a>
+            </ul>
+        </div>
+        <div id="mobile">
+            <a href="cart.html"><i class="fa-solid fa-bag-shopping"></i></a>
+            <i id="bar" class="fa-solid fa-bars"></i>
+        </div>
+    </section>
         <main>
 
             <div class="contenedor__todo">
@@ -42,9 +62,9 @@ include './conex.php';
                     </form>
 
                     <!--Register-->
-                    <form action="carga.php" method="post" class="formulario__register" method="get">
+                    <form action="cargados.php" method="post" class="formulario__register" method="get">
                         <h2>Regístrarse</h2>
-                        <input type="text" name="nombre_c" placeholder="Nombre Copleto" required>
+                        <input type="text" name="nombre_c" placeholder="Nombre Completo" required>
                         <input type="text" name="email" placeholder="Correo Electronico" required>
                         <input type="text" name="usuario" placeholder="Usuario" required>
                         <input type="password" name="contra" placeholder="Contraseña" required>
